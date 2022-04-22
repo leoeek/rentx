1afe2cdba529c49de8d5f81b55a88516b803051d
Leo <leonardolp@gmail.com>
2022-04-22 11:14:31 -0300

    add Sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -3,6 +3,8 @@ import "dotenv/config";
 import "express-async-errors";
 import cors from "cors";
 import swaggerUi from "swagger-ui-express";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 
 import createConnection from "@shared/infra/typeorm";
 import { AppError } from "@shared/errors/AppError";
@@ -17,7 +19,22 @@ createConnection();
 const app = express(); 
 
 app.use(rateLimiter);
+
+Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+      new Sentry.Integrations.Http({ tracing: true }),
+      new Tracing.Integrations.Express({ app }),
+    ],
+  
+    tracesSampleRate: 1.0,
+});
+app.use(Sentry.Handlers.requestHandler());
+
 app.use(express.json());
+
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
@@ -25,17 +42,25 @@ app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(cors());
 app.use(router);
-app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-        return response.status(err.statusCode).json({
-            message: err.message
-        })
-    }
 
-    return response.status(500).json({
+app.use(Sentry.Handlers.errorHandler())
+
+app.use(
+    (err: Error, request: Request, response: Response, next: NextFunction) => {
+      if (err instanceof AppError) {
+        return response.status(err.statusCode).json({
+          message: err.message,
+        });
+      }
+      return response.status(500).json({
         status: "error",
-        message: `Internal server error - ${err.message}`,
-    });
-})
+        message: `Internal server error ${err.message}`,
+      });
+    }
+);
+// app.use(function onError(err, req, res, next) {
+//     res.statusCode = 500;
+//     res.end(res.sentry + "\n");
+// });
 
 export { app };
